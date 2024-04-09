ae18a6b13789ec4c35c51f1977bc18bb280325d6
ricsaittan <151973832+ricsaittan@users.noreply.github.com>
2024-04-09 17:35:45 -0600

    Add files via upload

---
+++ IController.cs
@@ -0,0 +1,29 @@
+class interface IController
+{
+    double TaskCompletedPercentage();
+
+    Tasks [] TakstoArrays();
+
+    Tasks AddTask();
+
+    Tasks RemoveTask();
+
+    Tasks ModifyTask();
+
+    Tasks NoOfTasks();
+
+    Tasks IsEmpty();
+
+    Events [] EventArrays();
+
+    Events AddTask();
+
+    Events RemoveTask();
+
+    Events ModifyTask();
+
+    Events NoOfTasks();
+
+    Events IsEmpty();
+
+}
